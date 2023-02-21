9a9d12b447175e2435dfb537a295504ae327a21e
Brice Leahy <briceleahy@yahoo.com>
2023-02-21 09:27:26 -0800

    working rapper api

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+
+    '21 savage': {
+        'age': 28,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'age': 28,
+        'birthName': 'Chancelor Jonathan Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'unknown':{
+        'age': 'unknown',
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response) => {
+    const rapperName = request.params.name.toLowerCase()
+    console.log( rappers[rapperName] )
+    if( rappers[rapperName] ){
+        response.json(rappers[rapperName])
+    } else {
+        response.json(rappers['unknown'])
+    }
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+})
